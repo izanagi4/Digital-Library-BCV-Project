cca893d6ac026bc941269ffd27bb4353002a6b14
izanagi4 <teddygunnar@gmail.com>
2021-01-18 08:59:57 +0700

    lots of fix

--- src/Login.js
+++ src/Login.js
@@ -10,6 +10,7 @@ function Login() {
   const [passwordLogin, setPasswordLogin] = useState("");
 
   const [loginStatus, setLoginStatus] = useState("");
+
   Axios.defaults.withCredentials = true;
   const register = () => {
     Axios.post("https://bcv-server-mysql.herokuapp.com/register", {
@@ -35,7 +36,7 @@ function Login() {
         window.location.reload();
       } else {
         alert(`Welcome ${loginStatus}`);
-        window.location.href = "/";
+        console.log(response);
       }
     });
   };
@@ -114,6 +115,7 @@ function Login() {
           }}
         />
         <button onClick={login}>Login!</button>
+        <h1>{loginStatus} is logged in</h1>
         <h6 onClick={ChangetoRegist}>Haven't registered yet? Create one!</h6>
         <ul className="social-media">
           <li>
